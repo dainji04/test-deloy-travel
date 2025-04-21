680bcd6730bb20c514f11306029e64373604f65f
dainji04 <ndai31072004@gmail.com>
2025-04-21 21:48:05 +0700

    complete

---
+++ assets/Blog-B72g_7hy.js
@@ -0,0 +1,1 @@
+import{P as o}from"./Pagination-BdV8MU5L.js";import{d as e,b as t,i as a,o as n}from"./index-DSUy4WDE.js";const s={class:"contact"},p=e({__name:"Blog",setup(c){return(i,r)=>(n(),t("div",s,[a(o,{title:"Blog",link:["home","blog"],pagination:["Homepage","Blog"]})]))}});export{p as default};
